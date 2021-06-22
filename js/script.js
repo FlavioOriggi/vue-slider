a9a291ee7a0b6b9434f15eddeaf2de7117fd9097
Flavio <flavio.origgi@outlook.it>
2021-06-22 16:18:19 +0200

    devo sistemare la "nav"

--- js/script.js
+++ js/script.js
@@ -2,16 +2,29 @@ const app = new Vue(
     {
         el: '#app',
         data: {
-            data: {                
-                photos: [
-                    'img/01.jpg',
-                    'img/02.jpg',
-                    'img/03.jpg',
-                    'img/04.jpg'
-                ]
-            },
+            contatore: 0,
+            photos: [
+                'img/01.jpg',
+                'img/02.jpg',
+                'img/03.jpg',
+                'img/04.jpg'
+            ]
         },
-        
+        methods: {
+            nextPhoto(){                
+                (this.contatore == this.photos.length - 1) ? this.contatore = 0 : this.contatore++;
+            },
+            prevPhoto(){
+                if(this.contatore == 0){
+                    this.contatore = this.photos.length - 1;                    
+                } else {
+                    this.contatore--;
+                }
+            },
+            cambiaPhoto(indice){
+                this.contatore = indice;
+            }
+        }
 
     }
 );
