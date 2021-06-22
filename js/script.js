ad56ca9705a3fe047775e5bd7dd431126cc98422
Flavio <flavio.origgi@outlook.it>
2021-06-22 14:42:20 +0200

    add pulsanti esterni, img ed inizio del Vue

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@ const app = new Vue(
     {
         el: '#app',
         data: {
-            
+            data: {                
+                photos: [
+                    'img/01.jpg',
+                    'img/02.jpg',
+                    'img/03.jpg',
+                    'img/04.jpg'
+                ]
+            },
         },
         
 
